156f0c0968ce239b05c710fd1d6ff4165f09168f
FTapia <ftapia@promad.com.mx>
2023-09-17 23:54:25 -0600

    materialapp in auth

--- src/app/usuarios/auth.service.ts
+++ src/app/usuarios/auth.service.ts
@@ -36,7 +36,7 @@ export class AuthService {
   login(usuario: Usuario): Observable<any> {
     const urlEndpoint = 'http://localhost:8080/oauth/token';
 
-    const credenciales = btoa('angularapp' + ':' + '12345');
+    const credenciales = btoa('materialapp' + ':' + '12345');
 
     const httpHeaders = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
